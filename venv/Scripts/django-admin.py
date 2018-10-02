ca1a7314247620146d1a64b54539bf453b31c47f
Gera <gera_ivanoff@mail.ru>
2018-10-02 12:50:05 +0300

    Убраны не нужные файлы из проекта

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Александр\PycharmProjects\project_distribdb\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
