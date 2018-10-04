ba6d66ca3886cafa099b512163ff9ca71e942c9b
Gera <gera_ivanoff@mail.ru>
2018-10-05 00:20:02 +0300

    Добавление в админку "Lang_types" и "OS_type"

--- distribdb/admin.py
+++ distribdb/admin.py
@@ -5,3 +5,5 @@ from . import models
 admin.site.register(models.drop_device)
 admin.site.register(models.hasp_keys)
 admin.site.register(models.hardlock_keys)
+admin.site.register(models.Lang_types)
+admin.site.register(models.OS_type)
